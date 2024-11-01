90a26edc99bf049128cc2b81f110959835d4f9b8
Art21042147 <art21042147@gmail.com>
2024-11-01 12:07:29 +0300

    message ids cache

--- handlers/staff_choose.py
+++ handlers/staff_choose.py
@@ -22,20 +22,38 @@ async def display_brand_info(callback: CallbackQuery):
     text, markup = await brand_info_kb(brand_title)
     await callback.message.edit_text(text=text, reply_markup=markup)
 
+
+message_ids_cache = {}
+
 # color selection handler
 @choose_router.callback_query(F.data.startswith('color_'))
 async def process_color(callback: CallbackQuery):
     brand_title = callback.data.split('_')[1]
-    stone_data = await get_price_color(brand_title)
-    for media_file, message_text, color_data, price_rub in stone_data:
-        await callback.message.answer_photo(
-            photo=media_file,
-            caption=message_text,
-            reply_markup=get_color_kb(brand_title, color_data, price_rub)
-        )
 
-    await callback.message.answer(
-        text="Если Вам не подходят эти цвета,\n"
-             "Вы можете посмотреть цвета других производителей:",
-        reply_markup=brand_return_kb
-    )
+    # checks massage id in cache
+    if brand_title in message_ids_cache:
+        await callback.answer(
+            "Цвета для этого производителя уже были загружены.\n"
+            "Прокрутите пожалуйста ленту вверх, чтобы их увидеть.", show_alert=True)
+    else:
+        # if no messages in the cache, sends new data and save their ID
+        stone_data = await get_price_color(brand_title)
+        message_ids = []
+
+        for media_file, message_text, color_data, price_rub in stone_data:
+            msg = await callback.message.answer_photo(
+                photo=media_file,
+                caption=message_text,
+                reply_markup=get_color_kb(brand_title, color_data, price_rub)
+            )
+            # saves massage id
+            message_ids.append(msg.message_id)
+
+        # saves message ids in cache
+        message_ids_cache[brand_title] = message_ids
+
+        await callback.message.answer(
+            text="Если Вам не подходят эти цвета,\n"
+                 "Вы можете посмотреть цвета других производителей:",
+            reply_markup=brand_return_kb
+        )
